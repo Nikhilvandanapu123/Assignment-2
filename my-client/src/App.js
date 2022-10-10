daa5e56d79542851c77809a7c8c5ad94c180dbc8
Nikhil Vandanapu <nikhilvandanapu@Nikhils-Air.lan>
2022-10-09 20:40:07 -0400

    Third Commit

--- my-client/src/App.js
+++ my-client/src/App.js
@@ -6,8 +6,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-const shortText = "Sai Nikhil Vandanapu";
-const longText =
+const Short = "Sai Nikhil Vandanapu";
+const Long =
   "Hi Guys! This is Nikhil, I am pursing my masters at university at albany. I came to USA for Spring Semester, this is a New Experience for me Altogether, since i have been working in Infosys before coming here . My Strengths are Hardworking and leadership qualities.";
 
 function App() {
@@ -20,11 +20,11 @@ function App() {
           <img src={require("./img.jpeg")} width="300" height="300" class="d-inline-block align-top" alt="" />
           </Col>
           <Col >
-          <p className="Mynamefont">{shortText}</p>
-          <p>{longText}</p>
+          <p className="Mynamefont">{Short}</p>
+          <p>{Long}</p>
           </Col>
       </Row>
-    </Container>       
+    </Container>    
   </div>
   );
 }
